5560ab3da38713dde2e382c57e6a5afb9e161e0d
Marcos Sousa <marcosp.sousa@gmail.com>
2023-09-09 23:54:38 -0300

    first commit

---
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
@@ -0,0 +1,85 @@
+package com.example.fitnesstracker
+
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.fitnesstracker.R.*
+
+class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_imc)
+
+        editWeight = findViewById(id.edt_imc_weight)
+        editHeight = findViewById(id.edt_imc_height)
+
+        val btnSend: Button = findViewById(id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validateFields()) {
+                Toast.makeText(this, string.fields_message, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val imc = calculateImc(
+                editWeight.text.toString().toInt(),
+                editHeight.text.toString().toInt()
+            )
+
+            imcResponse(imc)
+        }
+    }
+
+    private fun imcResponse(imc: Double) {
+        val result = when {
+            imc < 15.0 -> string.imc_severely_low_weight
+            imc < 16.0 -> string.imc_very_low_weight
+            imc < 18.5 -> string.imc_low_weight
+            imc < 25 -> string.normal
+            imc < 30 -> string.imc_high_weight
+            imc < 35 -> string.imc_so_high_weight
+            imc < 40 -> string.imc_severely_high_weight
+            else -> string.imc_extreme_weight
+        }
+
+        AlertDialog.Builder(this)
+            .setTitle(getString(string.imc_response, imc))
+            .setMessage(result)
+            .setPositiveButton(
+                android.R.string.ok,
+                DialogInterface.OnClickListener() { dialog, _ ->
+                    dialog.dismiss()
+                }
+            )
+            .create()
+            .show()
+
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+            hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+    }
+
+    private fun validateFields(): Boolean {
+        val weight = editWeight.text.toString()
+        val height = editHeight.text.toString()
+
+        return weight.isNotEmpty() &&
+                weight.toInt() > 0 &&
+                height.isNotEmpty() &&
+                height.toInt() > 0
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        val heightInMeters = height / 100.0
+        return weight / (heightInMeters * heightInMeters)
+    }
+}
---
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.fitnesstracker
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnesstracker.R.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvMain: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+
+        val mainItems = populateButtons()
+
+        rvMain = findViewById(id.rv_main)
+        rvMain.adapter = MainAdapter(mainItems) { id ->
+            when (id) {
+                1 -> {
+                    startActivity(Intent(this@MainActivity, ImcActivity::class.java))
+                }
+
+                2 -> {
+                }
+            }
+        }
+        rvMain.layoutManager = GridLayoutManager(this, 2)
+
+    }
+
+    private fun populateButtons(): MutableList<MainItem> {
+        return mutableListOf<MainItem>().apply {
+            add(
+                MainItem(
+                    id = 1,
+                    drawableId = drawable.baseline_wb_sunny_24,
+                    textStringId = string.imc,
+                    color = Color.GREEN
+                )
+            )
+
+            add(
+                MainItem(
+                    id = 2,
+                    drawableId = drawable.baseline_visibility_24,
+                    textStringId = string.tmb,
+                    color = Color.YELLOW
+                )
+            )
+        }
+    }
+
+    private inner class MainAdapter(
+        private val mainItems: List<MainItem>,
+        private val onItemClickListener: (Int) -> Unit
+    ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            return MainViewHolder(layoutInflater.inflate(layout.main_item, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return mainItems.size
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val mainItem = mainItems[position]
+            holder.bind(mainItem)
+        }
+
+        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(menuItem: MainItem) {
+                itemView.findViewById<ImageView?>(id.item_img_icon).apply {
+                    setImageResource(menuItem.drawableId)
+                }
+
+                itemView.findViewById<TextView?>(id.item_txt_name).apply {
+                    setText(menuItem.textStringId)
+                }
+
+                itemView.findViewById<LinearLayout>(id.icon_container_imc).apply {
+                    setBackgroundColor(menuItem.color)
+                    setOnClickListener {
+                        onItemClickListener.invoke(menuItem.id)
+                    }
+                }
+
+            }
+        }
+    }
+}
